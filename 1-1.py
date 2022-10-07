0f9101854daf2bf728f4c6be52fba8b78ea44593
Alexander <113363612+alex000175@users.noreply.github.com>
2022-10-07 13:09:48 +0300

    Update 1-1.py

--- 1-1.py
+++ 1-1.py
@@ -1,3 +1,5 @@
+# запрос данных у пользователя и работа с переменными
+
 name = input("Input your name:\n")
 age = int(input("Input your age:\n"))
 print(f"{name}, you was born in {2022 - age} year!")
