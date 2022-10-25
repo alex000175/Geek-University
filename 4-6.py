daf79101235cfad5e6602bf23973d7e7299335fa
alex000175 <alex-roz@mail.ru>
2022-10-25 08:20:53 +0300

    lesson-4

--- 4-6.py
+++ 4-6.py
@@ -1,7 +1,6 @@
 # итератор, генерирующий целые числа, начиная с указанного;
 
-from sys import argv
-name, n = argv
+n = int(input('Введите целое число от 0 до 9: '))
 # n = 2
 def numbers_range(x):
     while True:
@@ -13,3 +12,17 @@ def numbers_range(x):
 a = numbers_range(int(n))
 for i in a:
     print(i)
+
+# итератор, повторяющий элементы некоторого списка, определённого заранее
+# -----------------------------------------------------------------------
+from itertools import cycle
+
+my_list = [2, 2, 2, 7, 23, 1, 44, 44, 3, 2, 10, 7, 4, 11]
+iterator = cycle(my_list)
+
+x = 0
+for i in iterator:
+    print(i)
+    x += 1
+    if x > 10:
+        break
